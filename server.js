cd8e8a1a347d2faac9b9e760082b350a2b1fb551
Favour Abatan <favour.abatan@sterling.ng>
2025-01-15 13:38:40 +0100

    posts

--- server.js
+++ server.js
@@ -4,7 +4,17 @@ const path = require("path");
 const app = express();
 
 //setup static folder
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public")));
+let posts = [
+  { id: 1, title: "Post One" },
+  { id: 2, title: "Post Two" },
+  { id: 3, title: "Post Three" },
+  { id: 4, title: "Post Four" },
+];
+
+app.get("/api/posts", (req, res) => {
+  res.json(posts);
+});
 
 // app.get("/", (req, res) => {
 //   res.sendFile(path.join(__dirname, "public", "index.html"));
