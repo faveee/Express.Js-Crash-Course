d7451356e93201d6cc532b94164999a845e60421
Favour Abatan <favour.abatan@sterling.ng>
2025-02-03 11:38:03 +0100

    Postman Utility

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const path = require("path");
+const port = process.env.PORT || 8000;
 
 const app = express();
 
 //setup static folder
 // app.use(express.static(path.join(__dirname, "public")));
+
 let posts = [
   { id: 1, title: "Post One" },
   { id: 2, title: "Post Two" },
@@ -12,8 +14,27 @@ let posts = [
   { id: 4, title: "Post Four" },
 ];
 
+// Get all posts
 app.get("/api/posts", (req, res) => {
-  res.json(posts);
+  const limit = parseInt(req.query.limit);
+
+  if (!isNaN(limit) && limit > 0) {
+    res.status(200).json(posts.slice(0, limit));
+  } else {
+    res.status(200).json(posts);
+  }
+});
+
+// Get single post
+app.get("/api/posts/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const post = posts.find((post) => post.id === id);
+
+  if (!post) {
+    res.status(404).json({ msg: `Post With ID Not Found` });
+  } else {
+    res.status(200).json(post);
+  }
 });
 
 // app.get("/", (req, res) => {
@@ -24,4 +45,4 @@ app.get("/api/posts", (req, res) => {
 //   res.sendFile(path.join(__dirname, "public", "about.html"));
 // });
 
-app.listen(8000, () => console.log("Server is running on port 8000"));
+app.listen(port, () => console.log(`Server is running on port ${port}`));
